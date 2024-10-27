60c652da2f18b2261aea2e2286cd81ecf8174768
leejinwoo26 <142082239+leejinwoo26@users.noreply.github.com>
2024-10-27 13:01:48 +0900

    Dxgi 에러정보 처리

--- DirectXGameEngine/Graphics.h
+++ DirectXGameEngine/Graphics.h
@@ -1,10 +1,52 @@
 #pragma once
 #include "DXWin.h"
+#include "DXException.h"
+#include <vector>
+#include "DxgiInfoManager.h"
 #include <d3d11.h>	
 
 
 class Graphics
 {
+public:
+	class Exception : public DXException
+	{
+		using DXException::DXException;
+	};
+	class HrException : public Exception
+	{
+	public:
+		HrException(int line, const char* file, HRESULT hr, std::vector<std::string> infoMsgs = {}) noexcept;
+		const char* what() const noexcept override;
+		const char* GetType() const noexcept override;
+		HRESULT GetErrorCode() const noexcept;
+		std::string GetErrorString() const noexcept;
+		std::string GetErrorDescription() const noexcept;
+		std::string GetErrorInfo() const noexcept;
+	private:
+		HRESULT hr;
+		std::string info;
+	};
+
+	class InfoException : public Exception
+	{
+	public:
+		InfoException(int line, const char* file, std::vector<std::string> infoMsgs) noexcept;
+		const char* what() const noexcept override;
+		const char* GetType() const noexcept override;
+		std::string GetErrorInfo() const noexcept;
+	private:
+		std::string info;
+	};
+	class DeviceRemovedException : public HrException
+	{
+		using HrException::HrException;
+	public:
+		const char* GetType() const noexcept override;
+	private:
+		std::string reason;
+	};
+
 public:
 	Graphics(HWND hWnd );
 	Graphics(const Graphics&) = delete;
@@ -13,6 +55,10 @@ public:
 	void EndFrame();
 	void ClearBuffer(float red, float green, float blue) noexcept;
 private:
+
+#ifndef NDEBUG
+	DxgiInfoManager infoManager;
+#endif
 	ID3D11Device* pDevice = nullptr;
 	IDXGISwapChain* pSwap = nullptr;
 	ID3D11DeviceContext* pContext = nullptr;
