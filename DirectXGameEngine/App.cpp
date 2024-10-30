70086e36217dbfe8ec4cc6c467c69d7890b4a198
leejinwoo26 <jwjw7626@gmail.com>
2024-10-31 00:13:55 +0900

    Gdi매니저 및 샘플러 그리고 텍스처 매핑 테스트

--- DirectXGameEngine/App.cpp
+++ DirectXGameEngine/App.cpp
@@ -2,10 +2,16 @@
 #include "Melon.h"
 #include "Pyramid.h"
 #include "Box.h"
+#include "Sheet.h"
+#include "SkinnedBox.h"
 #include <sstream>
 #include <algorithm>
 #include <memory>
 #include "DXMath.h"
+#include "Surface.h"
+#include "GDIPlusManager.h"
+
+GDIPlusManager gdipm;
 
 App::App(): 
     wnd(800,600,L"DX3D_GameEngine")
@@ -36,6 +42,16 @@ App::App():
 					gfx, rng, adist, ddist,
 					odist, rdist, longdist, latdist
 				);
+			case 3:
+				return std::make_unique<Sheet>(
+					gfx, rng, adist, ddist,
+					odist, rdist
+				);
+			case 4:
+				return std::make_unique<SkinnedBox>(
+					gfx, rng, adist, ddist,
+					odist, rdist
+				);
 			default:
 				assert(false && "bad drawable type in factory");
 				return {};
@@ -51,12 +67,13 @@ App::App():
 		std::uniform_real_distribution<float> bdist{ 0.4f,3.0f };
 		std::uniform_int_distribution<int> latdist{ 5,20 };
 		std::uniform_int_distribution<int> longdist{ 10,40 };
-		std::uniform_int_distribution<int> typedist{ 0,2 };
+		std::uniform_int_distribution<int> typedist{ 0,4};
 	};
 
 	Factory f(wnd.Gfx());
 	drawables.reserve(nDrawables);
-	std::generate_n(std::back_inserter(drawables), nDrawables, f);
+	std::generate_n(std::back_inserter(drawables), nDrawables, Factory{ wnd.Gfx() });
+
 
 	wnd.Gfx().SetProjection(DirectX::XMMatrixPerspectiveLH(1.0f, 3.0f / 4.0f, 0.5f, 40.0f));
 }
@@ -73,13 +90,13 @@ int App::Go()
 	}
 }
 
-void App::DoFrame()
+void App::DoFrame()  
 {
 	const auto dt = timer.Mark();
 	wnd.Gfx().ClearBuffer(0.07f, 0.0f, 0.12f);
 	for (auto& d : drawables)
 	{
-		d->Update(dt);
+		d->Update(wnd.kbd.KeyIsPressed(VK_SPACE) ? 0.0f : dt);
 		d->Draw(wnd.Gfx());
 	}
 	wnd.Gfx().EndFrame();
--- DirectXGameEngine/Window.cpp
+++ DirectXGameEngine/Window.cpp
@@ -140,9 +140,9 @@ LRESULT Window::HandleMsg(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noe
 	case WM_KEYDOWN:
 		break;
 	case WM_SYSKEYDOWN:
-		if (!(lParam & 0x40000000) || kbd.AutorepeatIsEnabled())
+		if (!(lParam & 0x40000000) || kbd.AutorepeatIsEnabled()) 
 		{
-			kbd.OnKeyPressed(static_cast<unsigned char> (wParam));
+			kbd.OnKeyPressed(static_cast<unsigned char>(wParam));
 		}
 		break;
 	case WM_KEYUP:
